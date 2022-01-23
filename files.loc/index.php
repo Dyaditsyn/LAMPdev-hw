a2b4a6f4a7f1906dc67fa1fe354855ee61c03580
Vyacheslav <dyaditsyn@gmail.com>
2022-01-24 00:14:38 +0200

    files hw v1.0 done

--- files.loc/index.php
+++ files.loc/index.php
@@ -1,5 +1,4 @@
 <?php
-
 //? Создать текстовый файл в любом редакторе, каждая строка которого содержит логин и пароль пользователя, отделенных пробелом в виде
 //? login pass
 //? login1 123456
@@ -7,31 +6,55 @@
 //? пользователь ввел логин и пароль, в случае правильного ввода вывести логин на экран и так же создать новый файл или перезаписать 
 //? значение в существующем, используя логин в качестве имени файла, в этот файл заносить число правильных попыток входа для данного пользователя.
 
-// receiving data from the form
-$login = $_POST['login'];
-$pwd = $_POST['pwd'];
+$login = '';
+$pwd = '';
+$count = 0;
+define("ROOT_PATH", dirname(__FILE__));
 
-$login = trim($login);
-$login = stripslashes($login);
-$login = strip_tags($login);
-$login = htmlspecialchars($login);
-
-$pwd = trim($pwd);
-$pwd = stripslashes($pwd);
-$pwd = strip_tags($pwd);
-$pwd = htmlspecialchars($pwd);
-
-
-if ((!empty($login)) && (!empty($pwd))) {
+if ((!empty($_POST['login'])) && (!empty($_POST['pwd']))) {
     echo "Request submitted <br>";
-} else {
-    echo "Error! Fields are mandatory and can't be empty! <br>";
+
+    $login = trim($_POST['login']);
+    $login = stripslashes($login);
+    $login = strip_tags($login);
+    $login = htmlspecialchars($login);
+
+    $pwd = trim($_POST['pwd']);
+    $pwd = stripslashes($pwd);
+    $pwd = strip_tags($pwd);
+    $pwd = htmlspecialchars($pwd);
+
+    if (strlen($login) > 0 || strlen($pwd) > 0) {
+        $userFile = fopen(ROOT_PATH . DIRECTORY_SEPARATOR . "users.txt", "r");
+
+        do {
+            $tempStrArr = explode(" ", fgets($userFile));
+            $currentLogin = trim($tempStrArr[0]);
+            $currentPwd = trim($tempStrArr[1]);
+            if (($currentLogin === $login) && ($currentPwd === $pwd)) {
+                echo "Hello, user $currentLogin!<br>";
+                if (file_exists(ROOT_PATH . DIRECTORY_SEPARATOR . "$login.txt")) {
+                    $count = (int) file_get_contents(ROOT_PATH . DIRECTORY_SEPARATOR . "$login.txt", "r+");
+                    file_put_contents(ROOT_PATH . DIRECTORY_SEPARATOR . "$login.txt", "");
+                }
+                file_put_contents(ROOT_PATH . DIRECTORY_SEPARATOR . "$login.txt", ++$count);
+                break;
+            } elseif ($currentLogin === $login) {
+                echo "User '$login' please re-check you password anr try again<br>";
+                break;
+            }
+        } while ((!feof($userFile)) || ($currentLogin === $login));
+
+        if (($currentLogin !== $login) && ($currentPwd !== $pwd)) {
+            echo "No such user '$login' in database. Please re-check your input<br>";
+        }
+
+        fclose($userFile);
+    } else {
+        echo "Invalid input! No empty fileds allowed<br>";
+    }
 }
-
-echo "login: " . $login . "password: " . $pwd . "<br>";
-
 ?>
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
