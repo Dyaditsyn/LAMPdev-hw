921d6cf96735e551c87c0c418c2486959991bd19
Vyacheslav <dyaditsyn@gmail.com>
2022-02-09 03:32:12 +0200

    typo fix

--- mysite.loc/functions.php
+++ mysite.loc/functions.php
@@ -1,16 +1,16 @@
-//<?php
-    function json_to_arr($path)
-    {
-        $users = [];
-        if (file_exists($path)) {
-            $users = file_get_contents($path);
-            $users = json_decode($users, true);
-        }
-        return !empty($users) ? $users : "Error users.json file opening";
+<?php
+function json_to_arr($path)
+{
+    $users = [];
+    if (file_exists($path)) {
+        $users = file_get_contents($path);
+        $users = json_decode($users, true);
     }
+    return !empty($users) ? $users : "Error users.json file opening";
+}
 
-    function generateSignature($username, $time)
-    {
-        $salt = '13wrwerwe44';
-        return sha1($username . $time . $salt);
-    }
+function generateSignature($username, $time)
+{
+    $salt = '13wrwerwe44';
+    return sha1($username . $time . $salt);
+}
